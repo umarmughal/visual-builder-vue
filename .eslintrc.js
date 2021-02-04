b4ce31f897b87e0cbe09450268aec544faf94082
Sellpixels Robot <nick@mediatec.org>
2021-02-04 11:44:57 +0200

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     "plugin:vue/vue3-recommended",
     "prettier",
-    "prettier/vue"
+    "prettier/vue",
   ],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "space-before-function-paren": "off",
     "comma-dangle": ["error", "always-multiline"],
-    "generator-star-spacing": "off"
-  }
+    "generator-star-spacing": "off",
+  },
 };
