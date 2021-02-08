c67d5c305de511d0e832743e8ac18c51913ae833
Sellpixels Robot <nick@mediatec.org>
2021-02-08 17:11:54 +0200

    ACL refactoring

--- src/@vb/components/ACL/index.vue
+++ src/@vb/components/ACL/index.vue
@@ -1,50 +1,53 @@
 <template>
-  <div v-if="this.show">
+  <div v-if="show">
     <slot />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { ref, onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import { notification } from 'ant-design-vue'
 import router from '@/router'
 
 export default {
   name: 'VbACL',
   props: {
-    redirect: Boolean,
-    roles: Array,
+    redirect: {
+      type: Boolean,
+      value: false,
+    },
+    roles: {
+      type: Array,
+      default: () => [],
+    },
   },
-  computed: {
-    ...mapGetters('user', ['user']),
-  },
-  data() {
-    return {
-      aclDefaultRedirect: '/auth/404',
-      aclRedirect: this.redirect || false,
-      aclRoles: this.roles || [],
-      show: false,
-    }
-  },
-  mounted() {
-    this.athorize()
-  },
-  methods: {
-    athorize() {
-      const defaultRedirect = this.aclDefaultRedirect
-      const redirect = this.aclRedirect
-      const roles = this.aclRoles
-      const role = this.user.role
-      const authorized = roles.includes(role)
-      this.show = authorized
-      if (!authorized && redirect) {
-        const url = typeof redirect === 'boolean' ? defaultRedirect : redirect
-        this.$notification.warning({
+  setup(props) {
+    const store = useStore()
+    const router = useRouter()
+    const defaultRedirect = '/auth/404'
+    const show = ref(false)
+    const role = computed(() => store.getters['user/user'].role)
+
+    const athorize = () => {
+      const authorized = props.roles.includes(role.value)
+      show.value = authorized
+      if (!authorized && props.redirect) {
+        const url = typeof props.redirect === 'boolean' ? defaultRedirect : props.redirect
+        notification.warning({
           message: 'Unauthorized Access',
           description: `You have no rights to access this page. <br /> Redirected to ${url}`,
         })
         router.push(url)
       }
-    },
+    }
+
+    onMounted(athorize)
+
+    return {
+      show,
+    }
   },
 }
 </script>
